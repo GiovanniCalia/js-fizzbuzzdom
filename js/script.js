13316ff0e93ad4d9174536e49588aa2d778ccbe1
GiovanniCalia <johnca@outlook.it>
2022-02-23 16:46:22 +0100

    Continuazione dell'esercizio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 let areaBoxes = document.querySelector(".box-area");
 
 
-for (index = 0; index <= 100; index++){
-    areaBoxes.innerHTML += `<div class="box">${index}</div>`;
-    console.log(index);
-    if (index % 2 == 0){
+for (index = 1; index <= 100; index++){
+    if (index % 3 == 0){
+        areaBoxes.innerHTML += `<div class="box green">fizz</div>`;
+    } else if (index % 5 == 0){
+        areaBoxes.innerHTML += `<div class="box yellow">buzz</div>`;
+    } else if (index % 15 == 0){
+        areaBoxes.innerHTML += `<div class="box red">buzz</div>`;
+    } else {
         areaBoxes.innerHTML += `<div class="box">${index}</div>`;
     }
 }
+
